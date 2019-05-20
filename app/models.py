78e2571d9e41c9ac2864698148f4dd695c2b4ea7
Garrett Greenfield <Greenfield_Garrett@cat.com>
2019-05-20 16:07:49 -0500

    part 15 - refactoring done

--- app/models.py
+++ app/models.py
@@ -1,21 +1,21 @@
 from datetime import datetime
-from time import time
-from app import app, db, login
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
 from hashlib import md5
+from time import time
+from flask import current_app
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
+from app import db, login
 
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
 
-followers = db.Table('followers',
+followers = db.Table(
+    'followers',
     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
 )
 
-class User(db.Model, UserMixin):
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -41,7 +41,7 @@ class User(db.Model, UserMixin):
     def avatar(self):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://avatars.dicebear.com/v2/identicon/{}.svg'.format(digest)
-    
+
     def follow(self, user):
         if not self.is_following(user):
             self.followed.append(user)
@@ -64,16 +64,23 @@ class User(db.Model, UserMixin):
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
             {'reset_password': self.id, 'exp': time() + expires_in},
-            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+            current_app.config['SECRET_KEY'],
+            algorithm='HS256').decode('utf-8')
 
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            id = jwt.decode(token, app.config['SECRET_KEY'],
+            id = jwt.decode(token, current_app.config['SECRET_KEY'],
                             algorithms=['HS256'])['reset_password']
         except:
             return
         return User.query.get(id)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
